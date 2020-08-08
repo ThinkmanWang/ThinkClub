21938deff79b98e0f2377ea75a3331ae6959db0d
Thinkman Wang <wangxf1985@gmail.com>
2020-08-08 21:05:49 +0800

    update

--- make_deal.py
+++ make_deal.py
@@ -18,32 +18,43 @@ g_nSuccess = 0
 g_lock = threading.Lock()
 g_threadPool = ThreadPool(256)
 
+g_lstMan = None
+g_nManCount = 0
+g_lstMenu = None
+g_lstManager = None
+
 def rand_deal():
     global g_nSuccess
+    global g_lstMan
+    global g_lstMenu
+    global g_lstManager
+    global g_nManCount
 
     while True:
-        lstMan = ManService.random_man(random.randint(10, 200))
-        g_logger.info("Random %d man" % (len(lstMan), ))
+        lstMan = []
+        setIndex = set()
 
-        lstMenu = MenuService.get_menus()
-        lstManager = ManagerService.get_managers()
+        nRandMan = random.randint(10, 200)
+        while len(setIndex) < nRandMan:
+            setIndex.add(random.randint(0, g_nManCount - 1))
+
+        for nIndex in setIndex:
+            lstMan.append(g_lstMan[nIndex])
+
+        g_logger.info("Random %d man" % (len(lstMan), ))
 
         for dictMan in lstMan:
 
             dictPlayList = {}
-            nPlayCnt = random.randint(1, len(lstMenu))
+            nPlayCnt = random.randint(1, len(g_lstMenu))
 
             while len(dictPlayList.keys()) < nPlayCnt:
-                dictMenu = random.choice(lstMenu)
+                dictMenu = random.choice(g_lstMenu)
                 if str(dictMenu["id"]) not in dictPlayList.keys():
                     dictPlayList[str(dictMenu["id"])] = dict(dictMenu)
 
             szDate = randomDate("2010-01-01 00:00:00", "2020-12-31 23:59:59")
-            dictManager = random.choice(lstManager)
-
-            # with g_lock:
-            #     g_nSuccess += 1
-            #     g_logger.info("[%d] => %s %s %d items" % (g_nSuccess, szDate, dictMan["name"], len(dictPlayList.keys()), ))
+            dictManager = random.choice(g_lstManager)
 
             g_nSuccess += 1
             g_logger.info("[%d] => %s %s %d items" % (g_nSuccess, szDate, dictMan["name"], len(dictPlayList.keys()),))
@@ -58,7 +69,22 @@ def rand_deal():
 
 
 def main():
-    # rand_deal()
+    global g_lstMan
+    global g_lstMenu
+    global g_lstManager
+    global g_nManCount
+
+    g_lstMan = ManService.get_mans()
+    g_lstMenu = MenuService.get_menus()
+    g_lstManager = ManagerService.get_managers()
+
+    if g_lstMan is None or len(g_lstMan) <= 0 \
+            or g_lstManager is None or len(g_lstManager) <= 0 \
+            or g_lstMenu is None or len(g_lstMenu) <= 0:
+        return
+
+    g_nManCount = len(g_lstMan)
+    rand_deal()
 
     for i in range(256):
         g_threadPool.apply_async(rand_deal)

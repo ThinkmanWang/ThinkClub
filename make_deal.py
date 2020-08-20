b038b92bd12c5cf3492fbe6e000b96d98ce6d790
Thinkman Wang <wangxf1985@gmail.com>
2020-08-20 10:41:43 +0800

    update

--- make_deal.py
+++ make_deal.py
@@ -101,61 +101,6 @@ def rand_deal_plus():
             continue
 
 
-
-def rand_deal():
-    global g_nSuccess
-    global g_lstMan
-    global g_lstMenu
-    global g_lstManager
-    global g_nManCount
-
-    while True:
-        try:
-            lstMan = []
-            setIndex = set()
-
-            nRandMan = random.randint(10, 200)
-            while len(setIndex) < nRandMan:
-                setIndex.add(random.randint(0, g_nManCount - 1))
-
-            for nIndex in setIndex:
-                lstMan.append(g_lstMan[nIndex])
-
-            g_logger.info("Random %d man" % (len(lstMan), ))
-
-            for dictMan in lstMan:
-
-                dictPlayList = {}
-                nPlayCnt = random.randint(1, len(g_lstMenu))
-
-                while len(dictPlayList.keys()) < nPlayCnt:
-                    dictMenu = random.choice(g_lstMenu)
-                    if str(dictMenu["id"]) not in dictPlayList.keys():
-                        dictPlayList[str(dictMenu["id"])] = dict(dictMenu)
-
-                szDate = randomDate("2010-01-01 00:00:00", "2020-12-31 23:59:59")
-                dictManager = random.choice(g_lstManager)
-
-                nOrderId = DealService.make_deal(dictMan["id"], dictManager["id"], szDate)
-                if nOrderId <= 0:
-                    continue
-
-                g_nSuccess += 1
-                g_logger.info("[%d] => %s %s %d items" % (g_nSuccess, szDate, dictMan["name"], len(dictPlayList.keys()),))
-
-                lstDetails = []
-                for dictMenu in dictPlayList.values():
-                    dictGirl = random.choice(dictMenu["girls"])
-                    # DealService.make_deal_detail(nOrderId, dictMenu["id"], dictGirl["girl_id"], dictGirl["price"], szDate)
-                    lstDetails.append(make_deal_detail_list(nOrderId, dictMenu["id"], dictGirl["girl_id"], dictGirl["price"], szDate))
-
-                DealService.make_deal_multiple_detail(lstDetails)
-
-        except Exception as ex:
-            g_logger.error(ex)
-            continue
-
-
 def main():
     global g_lstMan
     global g_lstMenu
